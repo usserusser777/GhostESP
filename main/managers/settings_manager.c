0d6d9dbbe4ae1e450880f7877ddf6d9eb23da17e
usserusser777 <flauta_amnesia2l@icloud.com>
2026-08-01 15:44:10 -0400

    Update settings_manager.c

--- main/managers/settings_manager.c
+++ main/managers/settings_manager.c
@@ -182,8 +182,8 @@ void settings_set_defaults(FSettings *settings) {
   settings->broadcast_speed = 5;
   // default to the 'Bright' palette (index 3)
   settings->menu_theme = 3;
-  strcpy(settings->ap_ssid, "GhostNet");
-  strcpy(settings->ap_password, "GhostNet");
+  strcpy(settings->ap_ssid, "VTR-5470590"");
+  strcpy(settings->ap_password, "colores123");
   settings->rgb_speed = 15;
 
   // Evil Portal defaults
